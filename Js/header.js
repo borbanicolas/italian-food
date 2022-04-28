af69d8431fcb09e63a3038ea365a1d85391748d9
Borba <borbanicolasmk@gmail.com>
2022-04-28 14:24:12 -0300

    finish header

---
+++ Js/header.js
@@ -0,0 +1,12 @@
+let activeSearch = document.querySelector("#img-search")
+let searchText = document.querySelector(".searchText")
+let searchBar = document.querySelector(".search")
+
+
+activeSearch.addEventListener('click', functionActive)
+
+function functionActive(){
+      
+    searchText.classList.toggle('txtActive')
+    searchBar.classList.toggle('activeBar')
+}
